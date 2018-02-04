f79a8e59dffddde22699e7ca4ca637fb1cf2ff99
GraysonRicketts <gricket@purdue.edu>
2018-02-04 17:55:57 -0600

    Add router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import Login from './Login';
 import registerServiceWorker from './registerServiceWorker';
+import Route from 'react-router-dom/Route';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={App}/>
+      <Route path='/basket' component={App}/>
+      <Route path='/login' component={Login}/>
+      {/* <Route path='/signup' component={Signup}/> */}
+    </Switch>
+  </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
