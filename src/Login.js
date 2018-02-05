15b91514d5b9f9400589f520177d8bd6a7458c15
GraysonRicketts <gricket@purdue.edu>
2018-02-04 18:36:36 -0600

    Add create account link

--- src/Login.js
+++ src/Login.js
@@ -28,6 +28,18 @@ class Login extends React.Component {
                 <input type="password"/>
               </div>
 
+                <div className='loginButton'>
+                  <input type="submit" value="Login"/>
+                  <div className="create">
+                    <a href='#'><p>Create an account</p></a>
+                  </div>
+                </div>
+
+                
+              </div>
+
+              <div className="oauth">
+                <input type="button" value="Google"/>
               <input type="button" value="Facebook"/>
               <input type="button" value="Google"/>
 
