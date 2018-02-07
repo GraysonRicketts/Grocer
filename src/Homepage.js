2a9504d27a37b9528d82986f7f929913bfe075bf
GraysonRicketts <gricket@purdue.edu>
2018-02-06 20:12:36 -0600

    Add homepage

---
+++ src/Homepage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './Homepage.css';
+import Footer from './Footer';
+import Header from './Header';
+
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+
+        <Header title={''}/>
+
+          <div className='home'>
+            <h1>
+              Grocery shopping made easy
+            </h1>
+
+            <div className="subTitleArea">
+              <h3>
+                We're focused on making the trip to the grocery store effortless.
+              </h3>
+
+              <input type="submit" value="Sign up for free"/>
+            </div>
+          </div>
+
+        <Footer />
+
+      </div>
+    );
+  }
+}
+
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import './index.css';
 import App from './App';
 import Login from './Login';
 import Signup from './Signup';
+import Homepage from './Homepage';
 import registerServiceWorker from './registerServiceWorker';
 import Route from 'react-router-dom/Route';
 
 ReactDOM.render((
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={App}/>
+      <Route exact path='/' component={Homepage}/>
       <Route path='/basket' component={App}/>
       <Route path='/login' component={Login}/>
       <Route path='/signup' component={Signup}/>
