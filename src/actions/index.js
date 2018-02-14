9fbead059d3534899b0671fc412f338aa9e98d36
GraysonRicketts <gricket@purdue.edu>
2018-02-13 23:07:15 -0600

    Add actions

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+let nextItemId = 0;
+export const addItem = (
+  category,
+  name,
+  number,
+  size,
+  note
+) => {
+  return { 
+    type: 'ADD_ITEM',
+    id: nextItemId++,
+    category,
+    name,
+    number,
+    size,
+    note
+  }
+};
+
+export const toggleItem = id => {
+  return {
+    type: 'TOGGLE_ITEM',
+    id
+  };
+};
