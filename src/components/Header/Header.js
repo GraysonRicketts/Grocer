ceca292feade401e51d2518d9f790c8814283baa
GraysonRicketts <gricket@purdue.edu>
2018-02-14 20:19:32 -0600

    Make header presentational

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,16 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/basket">Basket</a></li>
-            <li><a href="/account">Account</a></li>
-            <li><a href="/login">Login</a></li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
-}
+const Header = () => (
+  <div className="header">
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/basket">Basket</a></li>
+        <li><a href="/account">Account</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </nav>
+  </div>
+)
 
 export default Header;
