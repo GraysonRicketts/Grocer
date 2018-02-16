8ba4e33c46525c3080f274df4a63d29a98f23a62
GraysonRicketts <gricket@purdue.edu>
2018-02-15 20:10:39 -0600

    Change homepage button to link

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -4,6 +4,16 @@ import './Homepage.css';
 import Footer from './../Footer/Footer';
 import Header from './../Header/Header';
 
+const pStyle = {
+  fontSize: '1.2rem', 
+  padding: '5px 10px', 
+  borderRadius: '5px', 
+  width: '10rem',
+  backgroundColor: 'green',
+  margin: '0 auto',
+  textAlign: 'center'
+}
+
 const Homepage = () => (
   <div className="wrapper">
 
@@ -19,7 +29,11 @@ const Homepage = () => (
             We're focused on making the trip to the grocery store effortless.
           </h3>
 
-          <input type="submit" value="Sign up for free"/>
+          <div>
+            <a href="/signup" style={{textDecoration: 'none', color: 'white'}}>
+              <p style={pStyle}>Sign up for free</p>
+            </a>
+          </div>
         </div>
       </div>
 
