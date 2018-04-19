3e51b11aaccb471062ea74bf195613a7533ddb21
GraysonRicketts <gricket@purdue.edu>
2018-04-18 20:01:48 -0500

    Made homepage real

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -17,23 +17,37 @@ const pStyle = {
 const Homepage = () => (
   <div className="wrapper">
 
-    <Header title={''}/>
+    <Header />
 
       <div className='home'>
         <h1>
-          Grocery shopping made easy
+          This is a grocery list
         </h1>
 
-        <div className="subTitleArea">
-          <h3>
-            We're focused on making the trip to the grocery store effortless.
-          </h3>
-
-          <div>
+        <div>
             <a href="/signup" style={{textDecoration: 'none', color: 'white'}}>
               <p style={pStyle}>Sign up for free</p>
             </a>
           </div>
+
+        <div className="subTitleArea">
+          <h3>
+            Add some things to your grocery list, go to the store, check those things off, 
+            buy those things, take 'em home, eat those things. This is a grocery list. Get you
+            the things you need to not be hungry. 
+          </h3>
+
+          <h3>
+            Oh yeah, tell your Mom, tell your Dad, tell your sister, tell your neighbor, tell 
+            your friend about us. We worked hard to make this simple thing easy to use and 
+            want to make technology around food our full time job. Support a small business or 
+            hopefully support us once we're a large business. Do what you want. Make an igloo. I've
+            always wanted to but failed horribly when I was four so I've never tried again. It's hard; 
+            so maybe don't make an igloo but you should tell your Mom, tell your Dad, tell your
+            sister, tell your neighbor, tell your friend. Maybe they'll like us a enough to throw a 
+            candy bar's worth of money our way so we can leave our boring jobs and make fun things for you.
+          </h3>
+
         </div>
       </div>
 
